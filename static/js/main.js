61116d87014ef044133c65fa6fd80a63bc5b789e
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-08-04 19:40:41 +0200

    wip trying to catch metamsk events without reloading page

--- static/js/main.js
+++ static/js/main.js
@@ -2,16 +2,20 @@
 // Global variables
 const sendRequestButton = document.getElementById('sendRequestButton');
 let currentAccount = null;
-
+// const provider = new ethers.providers.Web3Provider(window.ethereum);
+window.ethereum.on('disconnect', async (clearAccount));
 // As soon the page is loaded, try to connect metamask
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
     // Enbale connection
     ethereum.request({ method: 'eth_requestAccounts' }).then(changeAccount);
+    // ethereum.on("accountsChanged", () => window.location.reload());
+    // ethereum.on("disconnect", () => window.location.reload());
     if(currentAccount === null){
       sendRequestButton.disabled = true;
       alert('Waiting...');
+      sendRequestButton.innerText = 'Connect to Metamask before';
     }
     // TODO: catch MetaMask disconnection to reload page
   }
@@ -23,8 +27,18 @@ window.addEventListener('DOMContentLoaded', () => {
     sendRequestButton.style.cursor = "not-allowed";
     alert('Please install MetaMask to access the page properly');
   }
+}
+)
+  // Return function of a non-async useEffect will clean up on component leaving screen, or from re-reneder to due dependency change
+  // window.ethereum.off('accountsChanged', accountWasChanged);
+  // window.ethereum.off('connect', getAndSetAccount);
+  window.ethereum.off('disconnect', clearAccount);
 
-});
+const clearAccount = () => {
+  setaccount('0x0');
+  console.log('clearAccount');
+  window.location.reload()
+};
 
 function changeAccount(accounts) {
   if (accounts.length === 0) {
